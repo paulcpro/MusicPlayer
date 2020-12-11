dba64a06f3c4d614540f7d282c4e65379d337e89
paulcpro <44520400+paulcpro@users.noreply.github.com>
2020-12-11 14:54:35 +0100

    Circular image buttons added with new button style

--- Custom Controls/HighCircularArtist.xaml.cs
+++ Custom Controls/HighCircularArtist.xaml.cs
@@ -29,12 +29,12 @@ namespace MusicPlayer.Custom_Controls
         {
             get
             {
-                return this.ArtistImage.Source;
+                return this.ArtistImage.ImageSource;
             }
 
             set
             {
-                this.ArtistImage.Source = value;
+                this.ArtistImage.ImageSource = value;
             }
 
         }
@@ -52,6 +52,18 @@ namespace MusicPlayer.Custom_Controls
             }
 
         }
+
+        //public Uri ImageSource
+        //{
+        //    get { return (Uri)GetValue(ImageSourceProperty); }
+        //    set { SetValue(ImageSourceProperty, value); }
+        //}
+
+        //// Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        //public static readonly DependencyProperty ImageSourceProperty =
+        //    DependencyProperty.Register("ImageSource", typeof(Uri), typeof(HighCircularArtist));
+
+
 
     }
 
